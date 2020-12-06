dce9912606446a3385766c195f04bd862cb03ed5
root <grpetr189853@gmail.com>
2020-12-06 22:15:04 +0200

    Assign test error

--- views/test/list.php
+++ views/test/list.php
@@ -77,7 +77,8 @@ $(\"body\").on(\"click\",\"#assign-table\", function(e){
 //           var keys = $('#w0').yiiGridView(\"getSelectedRows\");
            var keys = $($(\"input.\" + e.target.className)[0]).val();
            var test_id = getUrlParams(location.href);
-           var url = '". \yii\helpers\Url::toRoute(['test/assign-test'])."' + \"/id/\" + test_id.id;  
+           var url = '". \yii\helpers\Url::toRoute(['test/assign-test'])."' + \"/id/\" + test_id.id;
+           console.log(url);  
            var doDelete;
            if($($(\"input.\" + e.target.className)[0]).is(':checked')){
                 doDelete = false;           
