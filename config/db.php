c1c5aae009b133c2bddc4c515332be500630d760
root <grpetr189853@gmail.com>
2020-12-06 20:30:45 +0200

    Database credentials

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     // 'dsn' => 'mysql:host=localhost;dbname=TestHubIst.basic',
-    'dsn' => 'pgsql:ec2-54-247-94-127.eu-west-1.compute.amazonaws.com;port=5432;dbname=db1stnobm8ihkg',
+    'dsn' => 'pgsql:host=ec2-54-247-94-127.eu-west-1.compute.amazonaws.com;port=5432;dbname=db1stnobm8ihkg',
     'username' => 'jprzieqtsrnwsa',
     'password' => 'c8c7c4060d88f76e7568f8ce86a70664c8c35232abab8ef54fd842ae4446c598',
     'charset' => 'utf8',
